c2908cb8a9e596bff1f989d2892c2dd095195674
DJamesCPCC <djames26@lab.cpcc.edu>
2026-02-18 20:27:25 -0500

    started fizzbuzz1.html

---
+++ web115/scripts/fizzbuzz1.js
@@ -0,0 +1,54 @@
+// ensures that the page has fully loaded before running scripts
+document.addEventListener('DOMContentLoaded', () => {
+  /*
+   *  event listener for the submit button that will change the 
+   *  greeting message to a personalized one based on the name 
+   *  the user submitted.
+    */
+  document.getElementById("fizzbuzz1_form").addEventListener('submit', function (event) {
+
+    // stops the form from taking the default action (submitting and refreshing the page)
+    event.preventDefault();
+
+    const firstName = document.getElementById("first_name").value.trim();
+    const middleInitial = document.getElementById("middle_initial").value.trim();
+    const lastName = document.getElementById("last_name").value.trim();
+
+    let fullName = firstName;
+    if (middleInitial) {
+      fullName += ` ${middleInitial}.`;
+    }
+    if (lastName) {
+      fullName += ` ${lastName}`;
+    }
+
+    document.getElementById("greeting").innerHTML = `Welcome to Jackal Repairs, ${fullName}!`;
+    console.log("Message has been changed");
+  });
+
+  /*
+   * event listener for the submit button that will add another box 
+   * for the output of the fizzbuzz lines
+   *
+   */
+
+  document.getElementById("fizzbuzz1_form").addEventListener('submit', function (event) {
+
+    const main = document.getElementsByClassName("main-text")[0];
+    const newBox = document.createElement('div');
+    const list = document.createElement("ol");
+
+    newBox.classList.add("box");
+    newBox.classList.add("box");
+    newBox.id = "fizzbuzz0_list";
+    list.id = "data0";
+
+    main.appendChild(newBox);
+    newBox.appendChild(list);
+
+  }, { once: true });
+
+  //
+  // event listener for the submit button that will output a numbered lists from 1 to 140
+  // with a themed word. Every multiple of 3 will output and special word instead of the standard
+  // every multiple of 5 will output 
